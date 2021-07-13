72aa1891999fbbc48372b203311308c84fdb161d
williamsgp71 <williamsguerrero71@.com>
2021-07-12 23:02:18 -0500

    commit inicial2

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.odoo13.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
