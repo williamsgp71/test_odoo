558cdb42cc7abe0fc25c842d29d65732127eeb39
williamsgp71 <williamsguerrero71@.com>
2021-07-13 23:18:22 -0500

    se creo el modelo

--- models/models.py
+++ models/models.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from typing_extensions import Required
 from odoo import models, fields, api
 
 
 class Visit(models.Model):
+    '''
+    Developer Williasm Guerrero 
+    Se creo este modelo como test
+    '''
+    
     _name ='custom_crm.visit'
     _descripcion = 'Visit'
 
     name = fields.Char(string = 'Descripcion')
+    customer = fields.Char(string = 'Cliente')
+    date = fields.Datetime(string = 'Fecha')
+    type = fields.Selection([('P','Presencia'),('W','Whatsapp'),('T','telefonico')], string = 'Tipo', Required = True)
+    done = fields.Boolean(string = 'Realizada', readonly = True)
