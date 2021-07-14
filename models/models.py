b2c2f48d930501c3accdc7f5a03c12c5cc6d6172
williamsgp71 <williamsguerrero71@.com>
2021-07-13 23:05:41 -0500

    se agrego descripcion

--- models/models.py
+++ models/models.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class custom_crm(models.Model):
-#     _name = 'custom_crm.custom_crm'
-#     _description = 'custom_crm.custom_crm'
+class Visit(models.Model):
+    _name ='custom_crm.visit'
+    _descripcion = 'Visit'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string = 'Descripcion')
